77fff4590b412bf2c63245f6fc19d2a6114154d6
Tom Wardrop <tom@tomwardrop.com>
2018-04-03 14:10:37 +1000

    v1.0.0.

---
+++ README.md
@@ -0,0 +1,105 @@
+Sorted.js is a replacement for the popular and not-so-loved-anymore Sorttable.js. It's only purpose is to make sorting HTML tables as simple and easy as possible, with the bonus of also allowing specificed columns to be resized.
+
+It's designed to gracefully handle any changes made to the table it's bound to, e.g. adding or removing columns and rows, changing values.
+
+## Getting started
+Include `sorted.js` somewhere in your page:
+``` javascript
+<script type="text/javascript" src="sorted.js"></script>
+```
+
+Give it a table element to bind to:
+``` javascript
+// Ensure `mytable` exists before calling
+new Sorted(document.getElementById("mytable"))
+```
+
+And finally, be sure to add come CSS to provide some visual feedback, or use the included stylesheet:
+
+``` html
+<link rel="stylesheet" type="text/css" href="sorted.css">
+```
+
+## Features
+
+* Designed to handle dynamic changes to table columns, rows and cells.
+* No dependencies
+* No hardcoded CSS
+* No DOM injection.
+* Sorts tables with multiple <tbody> elements, sorting each as it's own independent collection.
+* Column resize capability
+
+## How To...
+
+### Exclude columns as sortable
+To exclude a column from being sortable, add the `data-no-sort` attribute to the column header.
+
+``` javascript
+<th data-no-sort>Comments</th>
+```
+
+### Specify sort function
+Sorted.js will guess an appropriate sort function based on the first value for each column, though you'll likely want to specify the sort function to use for certain columns. The available sort functions are:
+
+* sortAlpha - Sorts the column alphabetically.
+* sortNumeric - Sorts the column numerically. Strips non-numeric characters as to support a wide range of number formatting.
+* sortNatural - A combination of sortAlpha and sortNumeric, sorting numbers numerically rather than alphabetically. Stricter than sortNumeric when determining what is considered a number.
+* sortDate - Parses column values as dates and sorts accordingly.
+
+
+### Sort by custom value
+If the values in the table cells aren't appropriate for sorting, you can define an alternate sorting value by setting the `data-sort-value` attribute on the table cell.
+
+``` html
+<td data-sort-state="200">Two-hundred</td>
+<td data-sort-state="11">Eleven</td>
+<td data-sort-state="3000">Three-thousand</td>
+```
+
+
+### Change default sort direction
+For each column you would like to change the default sort direction for, simply preset the `data-sort-state` attribute on the column header:
+
+``` html
+<th data-sort-state="sorted">My Column</th>
+```
+
+This will make Sorted.js believe it's already sorted that column ascendingly, and will therefore reverse sort it the next time it's clicked.
+
+### Make a column resizable
+To make a column resizable, add the `data-resizable` attribute to the column header.
+
+``` html
+<th data-resizable>My Resizable Column</th>
+```
+
+If you'd like the resize handle to be bigger, set the resizeHandleWidth option to your desired width when instantiating Sorted.js. Be sure to update the stylesheet to match.
+
+```javascript
+new Sorted(document.getElementById("mytable"), {resizeHandleWidth: 8})
+```
+
+### Sort on load
+To sort the table on load, or otherwise trigger sorting programmatically (i.e. not on click), call .sort(), like so:
+
+```javascript
+var sorted = new Sorted(document.getElementById("mytable"))
+sorted.sort(1) // Sorts the second column
+```
+
+Or...
+```javascript
+var myTable = document.getElementById("mytable")
+new Sorted(myTable)
+myTable.sorted.sort(1) // Sorts the second column
+```
+
+```javascript
+var sorted = new Sorted(document.getElementById("mytable"))
+```
+
+### Lock rows to the top or the bottom
+The best way to keep certain rows at the top or bottom of your table, is to use semantic HTML. Anything you want to stay at the top should be put in <thead>, and anything you want to stay at the bottom of your table should be placed in <tfoot>. Sorted.js only sorts rows within <tbody>.
+
+## Feedback
+Please use Github's *Issues* tab to ask questions, report issues,  or provide feedback and suggestions.
---
+++ sorted.js
@@ -0,0 +1,160 @@
+var Sorted = function (table, opts = {}) {
+  opts.resizeHandleWidth = opts.resizeHandleWidth || 4
+  var sorted = this;
+
+  this.init = function () {
+    if (table.sorted) table.sorted.unbindEventListeners();
+    table.sorted = this;
+    this.bindEventListeners()
+  }
+
+  this.bindEventListeners = function () {
+    table.tHead.addEventListener("click", this.headerClickHandler)
+    table.tHead.addEventListener("mousedown", this.resizeColumnHandler)
+  }
+
+  this.unbindEventListeners = function () {
+    table.tHead.removeEventListener("click", this.headerClickHandler)
+  }
+
+  this.headerClickHandler = function (e) {
+    var th = e.target;
+    while (th && th.nodeName != 'TH') { th = th.parentElement }
+    if (th && th.dataset.noSort == undefined) {
+      // Ensure resize handle hasn't been clicked
+      if ((e.screenX - th.getBoundingClientRect().x) < (th.offsetWidth - (opts.resizeHandleWidth / 2))) {
+        var columnIndex = Array.prototype.indexOf.call(th.parentElement.cells, th)
+        sorted.sort(columnIndex, th.dataset.sortState == 'sorted')
+      }
+    }
+  }
+
+  this.resizeColumnHandler = function (e) {
+    var th = e.target;
+    if (th && th.nodeName == 'TH' && th.dataset.resizable != undefined) {
+      // Check if the resize handle has been grabbed.
+      if ((e.screenX - th.getBoundingClientRect().x) >= (th.offsetWidth - (opts.resizeHandleWidth / 2))) {
+
+        var dragStartPosition = e.clientX
+        var columnStartingWidth = parseInt(window.getComputedStyle(th).width)
+
+        function onMove (e) {
+          var newWidth = columnStartingWidth + (e.clientX - dragStartPosition)
+          th.style.width = newWidth + "px"
+        }
+
+        function onDragEnd () {
+          document.removeEventListener("mousemove", onMove)
+          document.removeEventListener("mouseup", onDragEnd)
+        }
+
+        document.addEventListener("mousemove", onMove)
+        document.addEventListener("mouseup", onDragEnd)
+      }
+    }
+  }
+
+  this.sort = function (columnIndex, reverse) {
+    table.dataset.sorted = true;
+    for(i = 0; i < table.tBodies.length; i++) {
+      var tbody = table.tBodies[i]
+      var values = [];
+      for(x = 0; x < tbody.rows.length; x++) {
+        var value = new String(this.getCellValue(i, x, columnIndex)) // Create String object so custom properties work
+        value._rowIndex = x
+        values.push(value)
+      }
+      values.sort(this.getSortFunc(columnIndex))
+      orderedRows = values.map( function (value) {
+        return tbody.rows[value._rowIndex]
+      })
+      this.setSortState(columnIndex, reverse)
+      if (reverse) orderedRows.reverse()
+      for(x = 0; x < orderedRows.length; x++) {
+        tbody.appendChild(orderedRows[x])
+      }
+    }
+  }
+
+  this.setSortState = function (columnIndex, reverse) {
+    var headers = this.getColumnHeaders()
+    for(i = 0; i < headers.length; i++) {
+      delete headers[i].dataset.sortState
+      if(i == columnIndex) headers[i].dataset.sortState = reverse ? 'rsorted' : 'sorted'
+    }
+  }
+
+  this.getSortFunc = function (columnIndex) {
+    var sortFunc = this.getColumnHeaders()[columnIndex].dataset.sortFunc
+    if (sortFunc) {
+      if (typeof this[sortFunc] != "function") {
+        throw("Sorting function "+sortFunc+" is invalid.")
+      }
+    } else {
+      // var values = this.getColumnValues(columnIndex)
+      var value = this.getCellValue(0,0,columnIndex)
+      Date.parse(value)
+      if (/^\d{1,4}[\/\.-]\d{1,2}[\/\.-]\d{1,4}$/.test(value)) { // E.g. 2017-02-11, 4/1/2016, 12-01-2011
+        sortFunc = "sortDate"
+      } else if (/^[$£¥₣₤₧€₹₩₴₯₮₲₱₳₵₭₪₫]?[0-9,.\s]+(%|st|nd|rd|th)?$/.test(value)) {
+        sortFunc = "sortNumeric"
+      } else {
+        sortFunc = "sortAlpha"
+      }
+    }
+    this.getColumnHeaders()[columnIndex].dataset.sortFunc = sortFunc
+    return this[sortFunc];
+  }
+
+  this.getColumnHeaders = function () {
+    return table.tHead.rows[0].cells
+  }
+
+  this.getCellValue = function (tbodyIndex, rowIndex, columnIndex) {
+    var cell = table.tBodies[tbodyIndex].rows[rowIndex].cells[columnIndex];
+    return cell.dataset.sortValue || cell.innerText;
+  }
+
+  this.getColumnValues = function (columnIndex) {
+    var values = []
+    for(var i = 0; i < table.tBodies.length; i++) {
+      var rowCount = table.tBodies[i].rows.length
+      for(var x = 0; x < rowCount; x++) {
+        values.push(this.getCellValue(i, x, columnIndex))
+      }
+    }
+    return values;
+  }
+
+  this.toFloat = function (str) {
+    return parseFloat(str.replace(/[^0-9.]/g, ''))
+  }
+
+  this.sortAlpha = function (a, b) {
+    return a.localeCompare(b);
+  }
+
+  this.sortNumeric = function (a, b) {
+    a = sorted.toFloat(a)
+    b = sorted.toFloat(b)
+    if (isNaN(a)) a = 0;
+    if (isNaN(b)) b = 0;
+    return a-b;
+  }
+
+  this.sortNatural = function (a, b) {
+    if(isNaN(a) || isNaN(b)) {
+      return sorted.sortAlpha(a,b);
+    } else {
+      return sorted.sortNumeric(a,b);
+    }
+  }
+
+  this.sortDate = function (a, b) {
+     a = new Date(a)
+     b = new Date(b);
+     return a - b;
+  }
+
+  this.init();
+}
